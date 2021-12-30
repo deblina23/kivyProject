5adb259e008dcfa6870e3977d15e0eee94ae8228
Deblina Ghosh <deblina.ghosh@lexmark.com>
2021-12-30 18:53:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class FirstKivyApp(App):
+
+    def build(self):
+        return Label(text="Hello Deblina")
+
+if __name__=="__main__":
+    FirstKivyApp().run()
